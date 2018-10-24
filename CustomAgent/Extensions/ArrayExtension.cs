3767a678e3f70dfe6c7b770258828b4e82a3d70c
Fredrik Sy <fredrik.sy@icloud.com>
2018-10-24 17:28:51 +0200

    Add jump point search files.

---
+++ CustomAgent/Extensions/ArrayExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoridor.AI
+{
+    static class ArrayExtension
+    {
+        public static void Fill<TSource>(this TSource[] source, TSource value)
+        {
+            for (int i = 0; i < source.Length; i++)
+                source[i] = value;
+        }
+    }
+}
